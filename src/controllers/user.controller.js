5a0e9bdd54cc088eedce07ed484995634d2dc120
Mayur Bodhare <mrbodhre7@gmail.com>
2024-10-05 13:45:24 +0530

    add user management controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -345,5 +345,7 @@ export {
     refreshAccessToken, 
     changeCurrentPassword, 
     getCurrentUser,
-    updateAccountDetails
+    updateAccountDetails, 
+    updateUserAvatar, 
+    updateCoverImage
 };
